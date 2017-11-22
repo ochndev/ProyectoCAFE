baa6a51757d0b931df5fbf0e25bf984b8d95f0e2
Hannibal <Hannibal@DESKTOP-K6O1777>
2017-11-22 01:58:23 +0100

    Splitter en proceso

--- src/proyectocafe/Splitter.java
+++ src/proyectocafe/Splitter.java
@@ -5,10 +5,55 @@
  */
 package proyectocafe;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author Hannibal
  */
 public class Splitter extends Transformer{
+    
+    Document doc;
+    Slots entrada,salida;
+    
+    public Splitter(Slots entrada, Slots salida){
+        this.entrada = entrada;
+        this.salida = salida;
+    }
+    
+    public void Splittear(){
+        
+        String xPathExpression = "/cafe_order/drinks/drink";
+        
+        for(int i=0; i<entrada.buffer.size() ; i++){
+            try {
+                doc = entrada.getDocument();
+                
+                // Preparación de xpath
+                XPath xpath = XPathFactory.newInstance().newXPath();
+                
+                // Consultas
+                NodeList nodos = (NodeList) xpath.evaluate(xPathExpression, doc, XPathConstants.NODESET);
+                for (int j=0;j<nodos.getLength();j++){
+			System.out.println(nodos.item(j).getNodeName()+" : " +
+                           nodos.item(j).getAttributes().getNamedItem("nombre"));
+		}
+                
+                nodos.item(i).getTextContent();
+                
+                salida.setDocument(doc);
+            } catch (XPathExpressionException ex) {
+                Logger.getLogger(Splitter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     
 }
